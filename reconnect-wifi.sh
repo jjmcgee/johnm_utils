f2ae7a7bfae5bca7b58b377a9daab563376dd179
John McGee <john@mcgeeweb.uk>
2019-11-14 20:20:43 +0000

    comited the change to reconnect-wifi.sh

--- reconnect-wifi.sh
+++ reconnect-wifi.sh
@@ -15,7 +15,7 @@ time=$(date "+%Y/%m/%d %H:%M:%S")
 ret=$(ping -c 2 -W 1 -I $NetCard $PingTarget | grep 'received' | cut -d ',' -f 2 | cut -d ' ' -f 2)
 
 if [ "$ret" == "0" ]; then
-    sudo ifdown wlan0 && sudo ifup wlan0
+    ifdown wlan0 && ifup wlan0
     echo "Try Reconnect: $?, $time" | tee -a $LogFile
 else
     if [ "$LogAll" == "true" ]; then
